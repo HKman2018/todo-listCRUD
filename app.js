43221e77d3e181005ecbe8c4390793a86346eb7e
kinting <kinting0130@gmail.com>
2019-06-20 23:05:19 +0800

    Add detail page

--- app.js
+++ app.js
@@ -53,7 +53,11 @@ app.post('/todos', (req, res) => {
 })
 //顯示一筆 Todo的詳細內容 3
 app.get('/todos/:id', (req, res) => {
-  res.send('顯示Todo的詳細內容')
+  //req.params.id
+  Todo.findById(req.params.id, (err, todo) => {
+    if (err) return console.log(err)
+    return res.render('detail', { todo: todo })
+  })
 })
 //新增一筆 Todo  4
 app.get('/todos', (req, res) => {
