3bb923e79b998ad391bd7fed8dbeb7a65213747f
kinting <kinting0130@gmail.com>
2019-07-02 01:00:33 +0800

    Add method-override and update to RESTful routes

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const app = express()
 const exphbs = require('express-handlebars')                       // 啟用 
 const mongoose = require('mongoose')
 const Todo = require('./models/todo')
+
+
+//into methodOverride
+const methodOverride = require('method-override')
+
+//into bodyParser
 const bodyParser = require('body-parser')
 
 
@@ -10,6 +16,9 @@ app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({ extended: true }))
 
+//setting method- override
+app.use(methodOverride('_method'))
+//mongoose connected
 mongoose.connect('mongodb://localhost/todo', { useNewUrlParser: true })
 
 // mongoose 連線後透過 mongoose.connection 拿到 Connection 的物件
@@ -67,7 +76,7 @@ app.get('/todos/:id/edit', (req, res) => {
   })
 })
 //修改Todo 6
-app.post('/todos/:id', (req, res) => {
+app.put('/todos/:id', (req, res) => {
   Todo.findById(req.params.id, (err, todo) => {
     if (err) return console.error(err)
     todo.name = req.body.name
@@ -83,7 +92,7 @@ app.post('/todos/:id', (req, res) => {
   })
 })
 //刪除Todo 7 
-app.post('/todos/:id/delete', (req, res) => {
+app.delete('/todos/:id/delete', (req, res) => {
   Todo.findById(req.params.id, (err, todo) => {
     if (err) return console.error(err)
     todo.remove(err => {
